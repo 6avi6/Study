6823d84ed028cb98105080e54ff5c61b9582397f
6avi6 <dawid.kakol.02@gmial.com>
2022-10-11 22:55:23 +0200

    stworzenie programu do rozwiazywania wielomianow ^2 i ^1

--- Liczenie ms0/Liczenie ms0.cpp
+++ Liczenie ms0/Liczenie ms0.cpp
@@ -1,20 +1,63 @@
-﻿// Liczenie ms0.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <math.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float a=2, b=0, c = 0;
+    //wprowadzanie
+    std::cout << "Liczymy :Ax^2+Bx+C=0\n";
+    std::cout << "Podaj A:\n";
+    std::cin >> a;
+    std::cout << "Podaj B:\n";
+    std::cin >> b;
+    std::cout << "Podaj C:\n";
+    std::cin >> c;
+
+    std::cout <<std::endl<< a<<"x(^2)+ "<<b<<"x+ "<<c << std::endl << std::endl;
+
+
+    
+    //kwadratowa funkcja
+    if (a != 0) {
+
+        float delta = (b * b) - 4 * (a * c);
+
+        if (delta > 0) {
+            
+
+            float x1 = (-b - sqrt(delta)) /( 2 * a);
+            float x2 = (-b + sqrt(delta)) / (2 * a);
+
+            std::cout << " Dwa miejsca zerowe: " << std::endl;
+            std::cout <<"x1 = " << x1 << std::endl;
+            std::cout <<"x2 = " << x2 << std::endl;
+        }
+        else if (delta == 0) {
+            float x = (-b ) / (2 * a);
+            std::cout << "Jedno miejsce zerowe x = " << x << std::endl;
+        
+        }
+        else if (delta < 0) {
+            std::cout << "Brak rozwian!" << std::endl;
+        }
+
+    }
+    //liniowa
+    else if (a == 0 && b!=0) {
+        std::cout << "Wynik jest rowny x: " << (-c) / b << std::endl;
+    }
+    
+
+    else if (a == 0 && b == 0) {
+        std::cout << "Brak rozwiazan" << std::endl;
+    }
+
+    else if (a == 0 && b == 0 && c == 0) {
+        std::cout << "Nieskoczenie wiele rozwiazan" << std::endl;
+    }
+
+
+
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
