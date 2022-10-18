6e04f094bfb9b2a0b122b8d0a99585af7451ca04
6avi6 <dawid.kakol.02@gmial.com>
2022-10-18 23:30:02 +0200

    zakonczenie szyfrowanie plotkowego (nie optymalne)

--- Szyfr Płotkowy/Szyfr Płotkowy.cpp
+++ Szyfr Płotkowy/Szyfr Płotkowy.cpp
@@ -10,13 +10,19 @@ int main()
 
     const int height{ 3 }, width{ 11 };
     char table[height][width];
-  
+
+    for (int i = 0; i < height; i++) {
+        for (int k = 0; k < width; k++) {
+            table[i][k] = NULL;
+        }
+        
+    }
 
     //szyfrowanie
     bool height_bounc = false;
 
     
-    
+    string cypher = "";
 
     int i{ 0 };
 
@@ -59,13 +65,32 @@ int main()
     }
 
 
-
+     //wyświetlanie i tworzenie napisu do zmiennej cypher
      for (int i = 0; i < height; i++) {
          for (int k = 0; k < width; k++) {
-             cout << " " << table[i][k]<<" ";
+             cout << " *" << table[i][k]<<"* ";
+             if (table[i][k] != NULL) {
+                 cypher = cypher + table[i][k];
+             }
          }
      cout<<endl;
      }
+
+     //zaszyfrowane
+    std::cout << cypher<<endl;
+    //char table2[height][width];
+    ////odszyfrowanie
+    //for (int i = height; 0 < height; i--) {
+    //    for (int k = 0; k < width; k++) {
+
+    //            cypher =
+    //        
+    //    }
+    //    
+    //}
+
+
+
     std::cout << "End!\n";
     
 }
