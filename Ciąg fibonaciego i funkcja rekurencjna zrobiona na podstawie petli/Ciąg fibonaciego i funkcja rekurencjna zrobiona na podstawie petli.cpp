1df7343725c53fcfc94d4f4ee5be7d1f7694f120
6avi6 <dawid.kakol.02@gmial.com>
2022-10-19 16:39:48 +0200

    dodanie ciagow

---
+++ Ciąg fibonaciego i funkcja rekurencjna zrobiona na podstawie petli/Ciąg fibonaciego i funkcja rekurencjna zrobiona na podstawie petli.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int fib(int n)
+{
+    if (n < 3)
+        return 1;
+
+    return fib(n - 2) + fib(n - 1);
+}
+
+int main()
+{
+    
+    int sum1 = 1, sum2 = 1,temp, n = 100;
+    for (int i = 0; i < n; i++) {
+        temp = sum1;
+        sum1 = sum1 + sum2;
+        cout << sum1<<endl;
+            sum2 = temp;
+    }
+   // cout<<fib(100);
+    
+}
+
